a74c416c4c9eefd095b475af3739c9c75fc96b18
Eganin <egorzaharin12@gmail.com>
2023-02-08 19:41:26 +0500

    add base navigation

---
+++ app/src/main/java/com/example/composemultimodulenavigation/main/BottomTabs.kt
@@ -0,0 +1,26 @@
+package com.example.composemultimodulenavigation.main
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.example.composemultimodulenavigation.R
+import com.example.dependency_provider.DependencyProvider
+
+enum class BottomBars(
+    @StringRes
+    val title: Int,
+    @DrawableRes
+    val icon: Int,
+    val route: String
+){
+
+    HOME(
+        title = R.string.home_label,
+        icon = R.drawable.baseline_home_24,
+        route=DependencyProvider.homeFeature().homeRoute()
+    ),
+    SETTINGS(
+        title = R.string.settings_label,
+        icon = R.drawable.baseline_settings_24,
+        route=DependencyProvider.homeFeature().homeRoute()
+    )
+}
